4e7e081f7664770c05cea041ba62ca667142b7b5
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2019-09-09 10:52:38 -0700

    Adds two integers

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//Kevin Buffardi
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+  int first = 0;
+  int second = 0;
+  int sum = 0;
+
+  cout<<"Enter a whole number: ";
+  cin>>first;
+  cout<<"Enter another whole number: ";
+  cin>>second;
+  sum = first + second;
+
+  cout<<"The sum is: "<<sum<<endl;
+
+  return 0;
+}
